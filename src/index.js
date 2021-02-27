a10ca5d2efdc27e677e216e03f84f7608249013f
Vebjørn Gilberg <vebjorngilberg@gmail.com>
2021-02-27 20:55:15 +0100

    firebase update

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import { App } from './app';
 import { FirebaseContext } from './context/firebase';
 
 const config = {
-  apiKey: 'AIzaSyDAU60M2FDHNsYt9uGMpRezw3FGVIX6Jrs',
+  apiKey: '',
   authDomain: 'netflix-c8ae9.firebaseapp.com',
   databaseURL: 'https://netflix-c8ae9.firebaseio.com',
   projectId: 'netflix-c8ae9',
   storageBucket: 'netflix-c8ae9.appspot.com',
-  messagingSenderId: '590575805931',
-  appId: '1:590575805931:web:f809092b6cd55306fa25ae',
+  messagingSenderId: '5',
+  appId: '1:590',
 };
 
 const firebase = window.firebase.initializeApp(config);
